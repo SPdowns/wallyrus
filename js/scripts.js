4381869b741f2b7f9d83f420eb258a0da00db9b2
Sean Downs <spdowns@protonmail.com>
2020-06-02 10:16:01 -0700

    change walrus to peek-a-boo. adds hidden elements and jquery show
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  $("p").click(function() {
-    alert("thisis aparagraph.");
-  });
-
-  $("img").click(function() {
-    alert("Img.");
+  $("p").click(function()) {
+    $(".walrus-showing").show
   });
 });
