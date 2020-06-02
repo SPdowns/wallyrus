1b937ad6ef8d91f959f72b51b25696def79e2d18
Sean Downs <spdowns@protonmail.com>
2020-06-01 20:51:04 -0700

    does $

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$("h1").click(function() {
+  alert("This is a header.");
+});
+
+$("p").click(function() {
+  alert("thisis aparagraph.");
+});
+
+$("img").click(function() {
+  alert("Img.");
+});
